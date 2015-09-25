f667e53eca7576bf7b9c35ef23fd6f251287f048
Sao <alexey.avalon@gmail.com>
2015-09-25 20:23:32 +0300

    + CBerAny

---
+++ src/types/berAny.h
@@ -0,0 +1,47 @@
+/*
+ * C++/QT Version
+ * Open source, 2015
+ * Author(s): Alexey Alyoshkin
+ *
+ * Based on Java version
+ * Copyright Fraunhofer ISE, 2011
+ * Author(s): Stefan Feuerhahn
+ *
+ * This file is part of jASN1.
+ * For more information visit http://www.openmuc.org
+ *
+ * jASN1 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * jASN1 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with jASN1.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "../berByteArrayOutputStream.h"
+
+class CBerAny
+{
+	quint32 m_Length;
+
+public:
+
+	CBerAny(): m_Length(0)
+	{}
+
+	CBerAny(quint32 length): m_Length(length)
+	{}
+
+	qint32 encode(CBerByteArrayOutputStream berOStream, bool expl)
+	{
+		return m_Length;
+	}
+
+};
