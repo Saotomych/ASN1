2223f4e9cff5972405c3ba709e721b2eeb4f8c77
Sao <alexey.avalon@gmail.com>
2015-10-02 15:51:22 +0300

    + CBerOctetString + CBerReal

---
+++ src/types/berOctetString.cpp
@@ -0,0 +1,63 @@
+#include "berOctetString.h"
+
+CBerOctetString::CBerOctetString()
+{
+	m_Identifier = s_Identifier;
+}
+
+CBerOctetString::CBerOctetString(QByteArray& octetString)
+{
+	m_Identifier = s_Identifier;
+	m_OctetString = octetString;
+}
+
+quint32 CBerOctetString::serialize(CBerByteArrayOutputStream& berOStream)
+{
+
+	berOStream.write(m_OctetString);
+
+	quint32 codeLength = m_OctetString.size();
+	codeLength += CBerLength::encodeLength(berOStream, codeLength);
+
+	return codeLength;
+}
+
+quint32 CBerOctetString::deserialize(QDataStream& iStream, CBerLength& length, quint32 codeLength)
+{
+
+	qint32 lenval = length.getVal();
+
+	if (lenval != 0)
+	{
+		char data[lenval];
+		if (iStream.readRawData(data, lenval) < lenval)
+		{
+			runtimeError("CBerOctetString::deserialize: error decoding");
+		}
+
+		codeLength += lenval;
+	}
+
+	return codeLength;
+}
+
+quint32 CBerOctetString::encode(CBerByteArrayOutputStream& berOStream, bool explct)
+{
+	quint32 codeLength = CBerBase::encode(berOStream, explct);
+
+	return codeLength;
+}
+
+quint32 CBerOctetString::decode(QDataStream& iStream, bool explct)
+{
+	int codeLength =  CBerBase::decode(iStream, explct);
+
+	return codeLength;
+}
+
+QString CBerOctetString::toString()
+{
+	QString str(m_OctetString);
+
+	return str;
+}
---
+++ src/types/berOctetString.h
@@ -0,0 +1,61 @@
+/*
+ * C++/QT Version
+ * Open source, 2015
+ * Author(s): Alexey Alyoshkin
+ *
+ * Based on Java version
+ * Copyright Fraunhofer ISE, 2011
+ * Author(s): Stefan Feuerhahn
+ *
+ * This file is part of jASN1.
+ * For more information visit http://www.openmuc.org
+ *
+ * jASN1 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * jASN1 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with jASN1.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef BER_OCTETSTRING
+#define BER_OCTETSTRING
+
+#include "../berBase.h"
+#include "../berByteArrayOutputStream.h"
+#include "../berIdentifier.h"
+#include "../berLength.h"
+
+class CBerOctetString: public CBerBase
+{
+
+protected:
+
+	QByteArray m_OctetString;
+
+public:
+	static CBerIdentifier s_Identifier(CBerIdentifier::UNIVERSAL_CLASS, CBerIdentifier::PRIMITIVE, CBerIdentifier::OCTET_STRING_TAG);
+
+	CBerOctetString();
+	CBerOctetString(QByteArray& octetString);
+
+	virtual ~CBerOctetString() {}
+
+	virtual quint32 serialize(CBerByteArrayOutputStream& berOStream);
+	virtual quint32 deserialize(QDataStream& iStream, CBerLength& length, quint32 codeLength);
+
+	quint32 encode(CBerByteArrayOutputStream& berOStream, bool explct);
+	quint32 decode(QDataStream& iStream, bool explct);
+
+	virtual QString toString();
+
+};
+
+#endif
